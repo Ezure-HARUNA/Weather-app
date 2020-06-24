138f576b1625b6bda4a20d3671b6359948ea59b2
ezure haruna <burimofumofu@gmail.com>
2020-06-25 07:43:26 +0900

    Updates

---
+++ precache-manifest.d1ac0462d2a6f92ad31ecc2c59e70ce4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac544a48e3b281b3008ca0f0c9079564",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "e49c446a40bf1cd690b1",
+    "url": "/weather-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ed9a1c63ac13a2e89b9",
+    "url": "/weather-app/static/js/2.e6c68a51.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.e6c68a51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e49c446a40bf1cd690b1",
+    "url": "/weather-app/static/js/main.a1ac5f89.chunk.js"
+  },
+  {
+    "revision": "8312f6281db4f4623ec7",
+    "url": "/weather-app/static/js/runtime-main.64ffec70.js"
+  }
+]);
